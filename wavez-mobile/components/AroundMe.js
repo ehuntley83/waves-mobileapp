2ea9b3a9d912964b16b7db71f601fa1cf0500b3b
Ernie Huntley <ehuntley83@yahoo.com>
2019-11-20 17:01:25 -0500

    Adding a MapView for the AroundMe screen (#2)
    * Adding react-native-maps to AroundMe view
    
    * Fetch the user's location upon loading AroundMe

--- wavez-mobile/components/AroundMe.js
+++ wavez-mobile/components/AroundMe.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import MapView from 'react-native-maps';
 import Constants from 'expo-constants';
 
 export default class AroundMe extends Component {
@@ -7,10 +10,58 @@ export default class AroundMe extends Component {
     title: 'Around Me',
   };
 
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+
   render() {
+    let text = 'Loading...';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>Around Me</Text>
+        {(this.state.errorMessage || !this.state.location)
+        ? <Text style={styles.paragraph}>{text}</Text>
+        : <MapView style={styles.mapStyle} 
+            initialRegion={{
+              latitude: this.state.location.coords.latitude,
+              longitude: this.state.location.coords.longitude,
+              latitudeDelta: 0.06,
+              longitudeDelta: 0.02}}>
+            <MapView.Marker
+              coordinate={{
+                latitude: this.state.location.coords.latitude,
+                longitude: this.state.location.coords.longitude
+              }}
+            />
+          </MapView>
+        }
       </View>
     );
   }
@@ -28,4 +79,8 @@ const styles = StyleSheet.create({
     color: '#becce7',
     fontSize: 20,
   },
+  mapStyle: {
+    width: '100%',
+    height: '100%',
+  }
 });
