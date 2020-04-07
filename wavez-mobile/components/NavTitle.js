c741fc679736fdb13802d150c80d4c9ee0c3485d
E. Huntley <ehuntley83@yahoo.com>
2020-04-07 13:40:06 -0400

    Missed MainMenu and NavTitle in the last commit

--- wavez-mobile/components/NavTitle.js
+++ wavez-mobile/components/NavTitle.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
 
-export default class NavTitle extends Component {
-  render() {
-    return (
-      <View style={styles.navTitleContainer}>
-        <Text style={styles.navTitleText}>Wavez </Text>
-        <Image style={styles.navTitleIcon} source={require('../assets/icon.png')} />
-        <Text style={styles.navTitleText}> Mobile</Text>
-      </View>
-    );
-  }
+const NavTitle = () => {
+  return (
+    <View style={styles.navTitleContainer}>
+      <Text style={styles.navTitleText}>Wavez </Text>
+      <Image style={styles.navTitleIcon} source={require('../assets/icon.png')} />
+      <Text style={styles.navTitleText}> Mobile</Text>
+    </View>
+  );
 }
+
+export default NavTitle;
 
 // todo: move these to a styles file
 const styles = StyleSheet.create({
